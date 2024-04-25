2ddce250c81fa8bc44a7d12348ff34d7353c91bd
KarinaPalopoli <111015887+KarinaPalopoli@users.noreply.github.com>
2024-04-25 04:42:04 +0000

    test rutinas

--- rutina_test1.go
+++ rutina_test1.go
@@ -1,99 +1,132 @@
 package rutinaDeEjercicios
 
 import (
-	"reflect"
-	"testing"
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+   
 )
 
-func TestAgregarRutina(t *testing.T) {
-	lista := NewListaDeRutinas()
+func TestCalcularDuracion(t *testing.T) {
+    // Prueba con Lista de ejercicios vacía
+    ejercicios := []*rutinaDeEjercicios.Ejercicio{}
+    assert.Equal(t, 0, rutinaDeEjercicios.CalcularDuracion(ejercicios), "La duración debe ser 0 para una lista vacía")
 
-	// Caso de prueba 1: Agregar una rutina válida
-	ejercicios1 := []*Ejercicio{
-		{nombre: "Flexiones", tiempo: 30, calorias: 100, tipoDeEjercicio: []string{"fuerza"}, dificultad: "principiante"},
-		{nombre: "Sentadillas", tiempo: 45, calorias: 150, tipoDeEjercicio: []string{"fuerza"}, dificultad: "principiante"},
-	}
-	err := lista.AgregarRutina("Rutina1", ejercicios1)
-	if err != nil {
-		t.Errorf("Error al agregar la rutina 1: %v", err)
-	}
+    // Prueba con Lista de ejercicios con elementos
+    ejercicios = []*rutinaDeEjercicios.Ejercicio{
+        {Nombre: "Flexiones", Tiempo: 30},
+        {Nombre: "Sentadillas", Tiempo: 45},
+        {Nombre: "Abdominales", Tiempo: 20},
+    }
+    duracionEsperada := 30 + 45 + 20 
+    assert.Equal(t, duracionEsperada, rutinaDeEjercicios.CalcularDuracion(ejercicios), "La duración no coincide con la esperada")
+ }
+ 
+ func TestAgregarRutina(t *testing.T) {
+    lista := rutinaDeEjercicios.NewListaDeRutinas()
 
-	// Caso de prueba 2: Intentar agregar una rutina con nombre duplicado
-	err = lista.AgregarRutina("Rutina1", ejercicios1)
-	if err == nil {
-		t.Errorf("Se esperaba un error al intentar agregar una rutina con nombre duplicado, pero no se recibió")
-	}
+    // prueba con una rutina válida
+    ejercicios1 := []*rutinaDeEjercicios.Ejercicio{
+        {Nombre: "Flexiones", Tiempo: 30},
+        {Nombre: "Sentadillas", Tiempo: 45},
+    }
+    err := lista.AgregarRutina("Rutina1", ejercicios1)
+    assert.NoError(t, err, "No se esperaba un error al agregar la rutina 1")
+
+    // prueba con nombre de rutina duplicado
+    err = lista.AgregarRutina("Rutina1", ejercicios1)
+    assert.Error(t, err, "Se esperaba un error al intentar agregar una rutina con nombre duplicado")
+    assert.EqualError(t, err, "la rutina ya existe")
+
+    // Agregando rutina sin ejercicios
+    err = lista.AgregarRutina("Rutina2", []*rutinaDeEjercicios.Ejercicio{})
+    assert.Error(t, err, "Se esperaba un error al intentar agregar una rutina sin ejercicios")
+    assert.EqualError(t, err, "una rutina debe contener al menos 1 ejercicio")
 }
 
-func TestEliminarRutina(t *testing.T) {
-	lista := NewListaDeRutinas()
+func TestBorrarRutina(t *testing.T) {
+    lista := rutinaDeEjercicios.NewListaDeRutinas()
+	//se crea una rutina con 1 ejercicio
+    ejercicios1 := []*rutinaDeEjercicios.Ejercicio{
+        {Nombre: "Flexiones", Tiempo: 30},
+        {Nombre: "Sentadillas", Tiempo: 45},
+    }
+    err := lista.AgregarRutina("Rutina1", ejercicios1)
+    assert.NoError(t, err, "No se esperaba un error al agregar la rutina 1")
 
-	ejercicios1 := []*Ejercicio{
-		{nombre: "Flexiones", tiempo: 30, calorias: 100, tipoDeEjercicio: []string{"fuerza"}, dificultad: "principiante"},
-		{nombre: "Sentadillas", tiempo: 45, calorias: 150, tipoDeEjercicio: []string{"fuerza"}, dificultad: "principiante"},
-	}
-	err := lista.AgregarRutina("Rutina1", ejercicios1)
-	if err != nil {
-		t.Errorf("Error al agregar la rutina 1: %v", err)
-	}
+    // eliminando una rutina existente
+    err = lista.BorrarRutina("Rutina1")
+    assert.NoError(t, err, "No se esperaba un error al eliminar la rutina 1")
 
-	// Caso de prueba 1: Eliminar una rutina existente
-	err = lista.BorrarRutina("Rutina1")
-	if err != nil {
-		t.Errorf("Error al eliminar la rutina 1: %v", err)
-	}
-
-	// Caso de prueba 2: Intentar eliminar una rutina que no existe
-	err = lista.BorrarRutina("Rutina1")
-	if err == nil {
-		t.Errorf("Se esperaba un error al intentar eliminar una rutina que no existe, pero no se recibió")
-	}
+    // eliminando una rutina inexistente
+    err = lista.BorrarRutina("Rutina1")
+    assert.Error(t, err, "Se esperaba un error al intentar eliminar una rutina que no existe")
+    assert.EqualError(t, err, "la rutina no existe")
 }
 
 func TestConsultarRutina(t *testing.T) {
-	lista := NewListaDeRutinas()
+    lista := rutinaDeEjercicios.NewListaDeRutinas()
 
-	ejercicios1 := []*Ejercicio{
-		{nombre: "Flexiones", tiempo: 30, calorias: 100, tipoDeEjercicio: []string{"fuerza"}, dificultad: "principiante"},
-		{nombre: "Sentadillas", tiempo: 45, calorias: 150, tipoDeEjercicio: []string{"fuerza"}, dificultad: "principiante"},
-	}
-	err := lista.AgregarRutina("Rutina1", ejercicios1)
-	if err != nil {
-		t.Errorf("Error al agregar la rutina 1: %v", err)
-	}
+    ejercicios1 := []*rutinaDeEjercicios.Ejercicio{
+        {Nombre: "Flexiones", Tiempo: 30},
+        {Nombre: "Sentadillas", Tiempo: 45},
+    }
+    err := lista.AgregarRutina("Rutina1", ejercicios1)
+    assert.NoError(t, err, "No se esperaba un error al agregar la rutina 1")
 
-	// Caso de prueba 1: Consultar una rutina existente
-	rutina, err := lista.ConsultarRutina("Rutina1")
-	if err != nil {
-		t.Errorf("Error al consultar la rutina 1: %v", err)
-	}
-	if !reflect.DeepEqual(rutina.ejerciciosTotales, ejercicios1) {
-		t.Errorf("Los ejercicios de la rutina 1 no coinciden con los esperados")
-	}
+    // consultar una rutina existente
+    rutina, err := lista.ConsultarRutina("Rutina1")
+    assert.NoError(t, err, "No se esperaba un error al consultar la rutina 1")
+    assert.Equal(t, "Rutina1", rutina.Nombre, "El nombre de la rutina no coincide")
 
-	// Caso de prueba 2: Intentar consultar una rutina que no existe
-	_, err = lista.ConsultarRutina("Rutina2")
-	if err == nil {
-		t.Errorf("Se esperaba un error al intentar consultar una rutina que no existe, pero no se recibió")
-	}
+    // intentar consultar una rutina inexistente
+    _, err = lista.ConsultarRutina("Rutina2")
+    assert.Error(t, err, "Se esperaba un error al intentar consultar una rutina que no existe")
+    assert.EqualError(t, err, "la rutina no existe")
 }
-func TestCalcularDuracion(t *testing.T) {
-	// Caso de prueba 1: Lista de ejercicios vacía
-	ejercicios := []*Ejercicio{}
-	duracion := calcularDuracion(ejercicios)
-	if duracion != 0 {
-		t.Errorf("Error en el caso de prueba 1. Se esperaba una duración de 0 pero se obtuvo %d", duracion)
-	}
 
-	// Caso de prueba 2: Lista de ejercicios con elementos
-	ejercicios = []*Ejercicio{
-		{nombre: "Flexiones", tiempo: 30},
-		{nombre: "Sentadillas", tiempo: 45},
-		{nombre: "Abdominales", tiempo: 20},
-	}
-	duracionEsperada := 30 + 45 + 20 // Duración total esperada es la suma de los tiempos de cada ejercicio
-	duracion = calcularDuracion(ejercicios)
-	if duracion != duracionEsperada {
-		t.Errorf("Error en el caso de prueba 2. Se esperaba una duración de %d pero se obtuvo %d", duracionEsperada, duracion)
-	}
+func TestModificarRutina(t *testing.T) {
+    lista := rutinaDeEjercicios.NewListaDeRutinas()
+
+    ejercicios1 := []*rutinaDeEjercicios.Ejercicio{
+        {Nombre: "Flexiones", Tiempo: 30},
+        {Nombre: "Sentadillas", Tiempo: 45},
+    }
+    err := lista.AgregarRutina("Rutina1", ejercicios1)
+    assert.NoError(t, err, "No se esperaba un error al agregar la rutina 1")
+
+    // modificando rutina existente
+    nuevosEjercicios := []*rutinaDeEjercicios.Ejercicio{
+        {Nombre: "Abdominales", Tiempo: 20},
+    }
+    err = lista.ModificarRutina("Rutina1", nuevosEjercicios)
+    assert.NoError(t, err, "No se esperaba un error al modificar la rutina 1")
+
+    // intentar modificar una rutina inexistente
+    err = lista.ModificarRutina("Rutina2", nuevosEjercicios)
+    assert.Error(t, err, "Se esperaba un error al intentar modificar una rutina que no existe")
+    assert.EqualError(t, err, "la rutina no existe")
+}
+
+func TestListarRutinas(t *testing.T) {
+    lista := rutinaDeEjercicios.NewListaDeRutinas()
+
+    // listar sin rutinas
+    rutinas, err := lista.ListarRutinas()
+    assert.Nil(t, rutinas, "Se esperaba nil al listar rutinas cuando no hay ninguna")
+    assert.Error(t, err, "Se esperaba un error al listar rutinas cuando no hay ninguna")
+    assert.EqualError(t, err, "no hay ninguna rutina para listar")
+
+    ejercicios1 := []*rutinaDeEjercicios.Ejercicio{
+        {Nombre: "Flexiones", Tiempo: 30},
+        {Nombre: "Sentadillas", Tiempo: 45},
+    }
+    err = lista.AgregarRutina("Rutina1", ejercicios1)
+    assert.NoError(t, err, "No se esperaba un error al agregar la rutina 1")
+
+    // listar con al menos una rutina
+    rutinas, err = lista.ListarRutinas()
+    assert.NotNil(t, rutinas, "Se esperaba una lista de rutinas al listar rutinas cuando hay al menos una")
+    assert.NoError(t, err, "No se esperaba un error al listar rutinas cuando hay al menos una")
+    assert.Equal(t, 1, len(rutinas), "Se esperaba una lista de rutinas con un elemento")
 }
